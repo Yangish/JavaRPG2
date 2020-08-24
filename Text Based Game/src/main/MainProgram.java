2c0eace1d724c04677f454dc82150ef53607921c
Yangish <yangishly@gmail.com>
2020-08-24 18:08:41 -0400

    huge update

--- Text Based Game/src/main/MainProgram.java
+++ Text Based Game/src/main/MainProgram.java
@@ -1,13 +1,35 @@
 package main;
 
+import player.PlayerClass;
+import enemies.Zombie;
+
+import java.util.Scanner;
+
 // This is going to be where the entire game is run. It might be a thread, it might not be. 
 public class MainProgram {
 
 	public static void main(String[] args) {
 		
-		System.out.println("This is the first ever version of my text based game!");
+		Scanner userInput = new Scanner(System.in);
 		
-		System.out.println("And this is my first ever update!");
+		System.out.println("What is your name?"); 
+		String name = userInput.next();
+		
+		PlayerClass player = new PlayerClass(name);
+		
+		System.out.println(name + "'s Health: " + player.getHealth());
+		System.out.println(name +"'s Armor: " + player.getArmor());
+		System.out.println(name + "'s Damage: " + player.getDamage());
+		System.out.println(name + "'s Name: " + player.getName());
+		
+		System.out.println();
+		
+		Zombie zombie = new Zombie();
+		
+		System.out.println("Zombie's Health: " + zombie.getHealth());
+		System.out.println("Zombie's Armor: " + zombie.getArmor());
+		System.out.println("Zombie's Damage: " + zombie.getDamage());
+		System.out.println("Zombie's Name: " + zombie.getName());
 	}
 
 }

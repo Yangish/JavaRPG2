915c4c7ef28cc7093527409891c10cb30f374383
Yangish <yangishly@gmail.com>
2020-08-24 17:03:30 -0400

    This is the first ever push for a project

---
+++ Text Based Game/src/main/MainProgram.java
@@ -0,0 +1,12 @@
+package main;
+
+// This is going to be where the entire game is run. It might be a thread, it might not be. 
+public class MainProgram {
+
+	public static void main(String[] args) {
+		
+		System.out.println("This is the first ever version of my text based game!");
+		
+	}
+
+}

5db5086b9979fcfb4e96b3c18f2138ba041b1259
Yangish <yangishly@gmail.com>
2020-08-24 21:17:21 -0400

    Created PartyMember1

---
+++ Text Based Game/src/friendlies/PartyMember1.java
@@ -0,0 +1,15 @@
+package friendlies;
+
+public class PartyMember1 {
+	
+	static String type; // this is to be called on whenever the party member is asked their class type. 
+	static int health; // a little bit smaller than the player's but since the player is a custom class 
+	static int armor; // the armor class that will aid in determining the damage of the party member
+	static int damage; // the damage that the party member will deal to enemies
+	
+	
+	public PartyMember1() {
+		
+	}
+
+}
